79814c2e4d2eed383a2644328c3d8632784d63f6
Drylian <109999325+drylian@users.noreply.github.com>
2025-09-26 02:14:02 -0300

    add:jsonDriver

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./ConfigJS";
 export * from "./drivers/env-driver";
+export * from "./drivers/json-driver";
 export * from "./factory";
 export * from "./types";
 // export * from './drivers/sqlite-driver';
